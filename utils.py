dd3bfe6cb2e42e78aa877c9e5c5bd8214979f2c9
Alba Mar Rodriguez Padilla <52015046+absrp@users.noreply.github.com>
2024-02-07 18:21:41 -0800

    Add files via upload

--- utils.py
+++ utils.py
@@ -32,6 +32,7 @@ def bootstrap_errors(xfeature, BUbin, classweightb, ax, minx, maxx, length, n_it
         # Resample with replacement
         resampled_xfeature, resampled_BUbin = resample(xfeature, BUbin, replace=True)
 
+
     # dealing with small number of unbreached gaps
         if len(resampled_BUbin.unique()) < 2:
             i = i-1 # update i to re-run this iteration
@@ -182,7 +183,7 @@ def build_logistic_regression(
 
     return probname, acc, pre, f1, roc, confusion_matrixi, BUbin, xfeature 
 
-def build_regression_double_bend_length(grouped,groupid,stress_typeYN,feature_type,axesid,minx,maxx,xlabel,ptsize):
+def build_regression_double_bend_length(grouped,groupid,stress_typeYN,feature_type,axesid,minx,maxx,xlabel,ptsize,colorline='slategrey',class_weightb=None):
 
     """
     This function calculates a logistic regression for a set of earthquake gate double bend length metrics from surface ruptures and plots the results.
@@ -196,14 +197,14 @@ def build_regression_double_bend_length(grouped,groupid,stress_typeYN,feature_ty
         feature = group[feature_type]
         BUbin = pd.get_dummies(group['Breached or unbreached'])
         BUbin = BUbin['breached']
-        group['logfeature'] = np.log10(group['Length (m) or angle (deg)'].astype('float')) 
-        xfeature = group['logfeature']
-        minx = np.log10(minx)
-        maxx = np.log10(maxx)
+        #group['logfeature'] = np.log10(group['Length (m) or angle (deg)'].astype('float')) 
+        xfeature = group['Length (m) or angle (deg)'].astype('float')
+        minx = minx
+        maxx = maxx
 
     palette = {'breached': 'teal', 'unbreached': 'darkorange'}
     if max(xfeature)>90:
-        sns.swarmplot(data=group,x=10**feature_type,y='Breached or unbreached',ax=axesid,size=ptsize,hue="Breached or unbreached",palette=palette,alpha=0.7)
+        sns.swarmplot(data=group,x=feature_type,y='Breached or unbreached',ax=axesid,size=ptsize,hue="Breached or unbreached",palette=palette,alpha=0.7)
     else:
         sns.swarmplot(data=group,x=feature_type,y='Breached or unbreached',ax=axesid,size=ptsize,hue="Breached or unbreached",palette=palette,alpha=0.7)
 
@@ -211,6 +212,35 @@ def build_regression_double_bend_length(grouped,groupid,stress_typeYN,feature_ty
     axesid.set_xlabel(xlabel)
     axesid.set_yticklabels(["Breached", "Unbreached"],rotation=90,va='center')
     axesid.get_legend().remove()
+
+    if max(group['Length (m) or angle (deg)'])>90:
+        bootstrap_errors(xfeature, BUbin, class_weightb, axesid, minx, maxx, True)
+    else: 
+        bootstrap_errors(xfeature, BUbin, class_weightb, axesid, minx, maxx, False)
+
+    probname = sklearn.linear_model.LogisticRegression(penalty='none',class_weight=class_weightb).fit(np.atleast_2d(xfeature).T,BUbin)
+
+    # tests
+    acc = accuracy_score(BUbin, probname.predict(np.atleast_2d(xfeature).T))
+    pre = precision_score(BUbin, probname.predict(np.atleast_2d(xfeature).T))
+    f1 = f1_score(BUbin, probname.predict(np.atleast_2d(xfeature).T))
+    roc =  roc_auc_score(BUbin, probname.predict_proba(np.atleast_2d(xfeature).T)[:,1])
+    confusion_matrixi = confusion_matrix(BUbin, probname.predict(np.atleast_2d(xfeature).T))
+
+    x = np.atleast_2d(np.linspace(minx, maxx, 10000)).T
+
+    if max(group['Length (m) or angle (deg)'])>90:
+        axesid.plot(x,probname.predict_proba(x)[:,1],color = colorline)
+        axesid.text(x[-10], -0.1, f'ROC={roc:.2f}', ha='right', va='top',fontsize=14)
+        axesid.set_xscale('log')
+    else:
+        axesid.text(x[-10], -0.1, f'ROC={roc:.2f}', ha='right', va='top',fontsize=14)
+        # axesid.text(x[-10], 0.1, r'$P_{50}$=' + str(int(x_at_threshold)) + '$^{\circ}$', ha='right', va='top', fontsize=14)
+        axesid.plot(x,probname.predict_proba(x)[:,1],color = colorline)
+
+    axesid.set_ylabel('Passing probability')
+    axesid.set_xlabel(xlabel)
+    axesid.set_yticklabels(["Breached", "Unbreached"],rotation=90,va='center')
 
     return BUbin, xfeature 
 
@@ -289,11 +319,11 @@ def build_cdf(grouped,groupid, stress_typeYN, length_or_angle, colorB,colorU, ax
     sns.ecdfplot(sortU,c=colorU,label=labelU,ax=axesid)
     axesid.set_xlabel(xlabel)
     axesid.set_ylabel('Proportion')
-    axesid.grid(color='lightgray', linewidth=0.5)
+    axesid.grid(color='lightgray', linewidth=0.5, alpha=0.5)
     axesid.legend()
 
 
-def build_cdf_lognorm(grouped,groupid, stress_typeYN, length_or_angle, colorB, colorU, axesid,xlabel,labelB, labelU):
+def build_cdf_lognorm(grouped, groupid, stress_typeYN, length_or_angle, colorB, colorU, axesid,xlabel,labelB, labelU):
 
     """
     This function generates CDFs for the geometry measured for each earthquake gate, separated into breached and unbreached features and restraining and releasing when available. We then fit log normal CDFs to the ECDF.
@@ -354,7 +384,7 @@ def build_cdf_lognorm(grouped,groupid, stress_typeYN, length_or_angle, colorB, c
     else: 
         KeyError("Feature must include a length or an angle")
 
-    # log normal fit breache
+
     sortB = np.sort(xvals_B)
     # calculate the proportional values of samples
     p = 1. * np.arange(len(xvals_B)) / (len(xvals_B) - 1) 
@@ -384,7 +414,12 @@ def build_cdf_lognorm(grouped,groupid, stress_typeYN, length_or_angle, colorB, c
 
     axesid.set_xlabel(xlabel)
     axesid.set_ylabel('Proportion')
-    axesid.legend(fontsize=8)
+    if groupid == 'stepover':
+        axesid.legend(fontsize=8)
+    else:
+        axesid.legend(fontsize=10)
+
+    axesid.grid(color='lightgray', linewidth=0.5, alpha=0.5)
 
 
 # make PDFs
@@ -493,6 +528,30 @@ def build_cdf_bend_lengths(grouped,groupid, stress_typeYN, featuretype, colorB,c
     axesid.set_xscale('log')
     axesid.set_ylabel('Proportion')
     axesid.legend()
+    axesid.legend(fontsize=10)
+
+
+    # log normal fit breached
+    yvals = np.arange(len(sortB)) / float(len(sortB)-1)
+    shape, loc, scale = lognorm.fit(sortB, floc=0, f0=1-yvals[-1])
+    xvals = np.linspace(min(sortB), max(sortB), 100)
+    cdf_fitted = lognorm.cdf(xvals, shape, loc, scale)
+    axesid.plot(xvals, cdf_fitted, color=colorB,linestyle=':')  
+
+    sortU = np.sort(xvals_U)
+    yvals = np.arange(len(sortU)) / float(len(sortU)-1)
+    shape, loc, scale = lognorm.fit(sortU, floc=0, f0=1-yvals[-1])
+    xvals = np.linspace(min(sortU), max(sortU), 100)
+    cdf_fitted = lognorm.cdf(xvals, shape, loc, scale)
+
+    if max(xvals)>90:
+        sns.ecdfplot(sortU,c=colorU,label=labelU,ax=axesid)
+        axesid.plot(xvals, cdf_fitted, color=colorU,linestyle=':') 
+        axesid.set_xscale('log')
+    else: 
+        sns.ecdfplot(sortU,c=colorU,label=labelU,ax=axesid)
+        axesid.plot(xvals, cdf_fitted, color=colorU,linestyle=':') 
+    axesid.grid(color='lightgray', linewidth=0.5, alpha=0.5)
 
 def kstest_variables(grouped,groupid, stress_typeYN, length_or_angle):
 
